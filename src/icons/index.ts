5aaea7b9fa9fd3f093d2921b8ac2f7f5e7b9af39
Jhonatan Kennedy <jhonatankennedy@live.com>
2024-09-21 14:58:45 -0300

    Feat: Add new icons to Icons component

--- src/icons/index.ts
+++ src/icons/index.ts
@@ -2,9 +2,15 @@ import { AiOutlineSearch } from 'react-icons/ai'
 
 import UnitOutlined from '../assets/unit.svg?react'
 import Logo from '../assets/logo.svg?react'
+import Location from '../assets/location.svg?react'
+import Component from '../assets/component.svg?react'
+import Asset from '../assets/asset.svg?react'
 
 const Outlined = {
     Unit: UnitOutlined,
+    Location,
+    Asset,
+    Component,
 }
 
 const Contained = {
