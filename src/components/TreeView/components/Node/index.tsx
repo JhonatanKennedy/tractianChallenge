acf019a041caa916135e8d1d014bc100f23a062e
Jhonatan Kennedy <jhonatankennedy@live.com>
2024-09-24 13:52:46 -0300

    Feat: TreeView component finished

--- src/components/TreeView/components/Node/index.tsx
+++ src/components/TreeView/components/Node/index.tsx
@@ -1,49 +1,95 @@
+import { useState } from 'react'
 import { AssetLeaf } from './components/AssetLeaf'
 import { ComponentLeaf } from './components/ComponentLeaf'
 import { LocationLeaf } from './components/LocationLeaf'
+import styles from './styles.module.scss'
+import { TreeNodeType } from '../../../../domain/Tree/Service/IService'
 
 export type NodeProps = {
-    name: string
-    isActive: boolean
-    hasChildren: boolean
-    type: 'asset' | 'component' | 'location'
-    onClickNode: () => void
+    onClickNode: (value: string) => TreeNodeType[]
+    node: TreeNodeType
 }
 
-export function Node({
-    type,
-    name,
-    isActive,
-    hasChildren,
-    onClickNode,
-}: NodeProps) {
-    if (type === 'location') {
+export function Node({ node, onClickNode }: NodeProps) {
+    const [children, setChildren] = useState<TreeNodeType[]>([])
+
+    function handleClickNode(id: string) {
+        if (children.length !== 0) {
+            setChildren([])
+            return
+        }
+        const childrenArray = onClickNode(id)
+        setChildren(childrenArray)
+    }
+
+    if (node.type === 'location') {
         return (
-            <LocationLeaf
-                name={name}
-                isActive={isActive}
-                onClickLocation={onClickNode}
-                hasChildren={hasChildren}
-            />
+            <div>
+                <LocationLeaf
+                    name={node.name}
+                    isActive={children.length !== 0}
+                    onClickLocation={() => handleClickNode(node.id)}
+                    hasChildren={node.hasChildren}
+                />
+                {children.length !== 0 && (
+                    <div className={styles['nodes-container']}>
+                        <div className={styles['trail-container']}>
+                            <div className={styles.trail} />
+                        </div>
+
+                        <div className={styles['children-container']}>
+                            {children.map((child) => (
+                                <Node
+                                    key={child.id}
+                                    node={child}
+                                    onClickNode={(id) => onClickNode(id)}
+                                />
+                            ))}
+                        </div>
+                    </div>
+                )}
+            </div>
         )
     }
 
-    if (type === 'asset') {
+    if (node.type === 'asset') {
         return (
-            <AssetLeaf
-                name={name}
-                isActive={isActive}
-                onClickAsset={onClickNode}
-                hasChildren={hasChildren}
-            />
+            <div>
+                <AssetLeaf
+                    name={node.name}
+                    isActive={children.length !== 0}
+                    onClickAsset={() => handleClickNode(node.id)}
+                    hasChildren={node.hasChildren}
+                />
+                {children.length !== 0 && (
+                    <div className={styles['nodes-container']}>
+                        <div className={styles['trail-container']}>
+                            <div className={styles.trail} />
+                        </div>
+
+                        <div className={styles['children-container']}>
+                            {children.map((child) => (
+                                <Node
+                                    key={child.id}
+                                    node={child}
+                                    onClickNode={() => onClickNode(child.id)}
+                                />
+                            ))}
+                        </div>
+                    </div>
+                )}
+            </div>
         )
     }
 
     return (
-        <ComponentLeaf
-            name={name}
-            isActive={isActive}
-            onClickComponent={onClickNode}
-        />
+        <div>
+            <ComponentLeaf
+                name={node.name}
+                // TODO this will be implemented whey the click on assets and components starts
+                isActive={false}
+                onClickComponent={() => onClickNode(node.id)}
+            />
+        </div>
     )
 }
---
+++ src/components/TreeView/index.tsx
@@ -0,0 +1,18 @@
+import { Node } from './components/Node'
+import { TreeNodeType } from '../../domain/Tree/Service/IService'
+import styles from './styles.module.scss'
+
+export type TreeViewProps = {
+    nodes: TreeNodeType[]
+    onClickNode: (id: string) => TreeNodeType[]
+}
+
+export function TreeView({ nodes, onClickNode }: TreeViewProps) {
+    return (
+        <div className={styles.container}>
+            {nodes.map((node) => (
+                <Node key={node.id} node={node} onClickNode={onClickNode} />
+            ))}
+        </div>
+    )
+}
