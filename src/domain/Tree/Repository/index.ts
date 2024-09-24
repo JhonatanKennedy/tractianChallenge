f4b2b758f41c46087a116033094c7a091e835e21
Jhonatan Kennedy <jhonatankennedy@live.com>
2024-09-24 13:21:42 -0300

    Refactor: Tree repository class

--- src/domain/Tree/Repository/index.ts
+++ src/domain/Tree/Repository/index.ts
@@ -1,8 +1,9 @@
 import { AxiosAdapter } from '../../infra/AxiosAdapter'
 import { _getLocations } from './functions/getLocations'
 import { _getAssets } from './functions/getAssets'
+import { IRepository } from './IRepository'
 
-export class Repository {
+export class Repository implements IRepository {
     constructor(private readonly adapter: AxiosAdapter) {}
 
     async getLocations(id: string) {
